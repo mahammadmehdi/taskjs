0745b2d0e875a0852a908c3bd0183e85eaf8a6ce
mahammadmehdi <mi7u4p9f7@code.edu.az>
2023-10-26 01:11:40 +0400

    function

--- js/script/script.js
+++ js/script/script.js
@@ -1,49 +1,69 @@
-// --------------------------------------------------task3-------------------------------------------------------------------------------
-// 1- den 1000e qeder  ededler icerisinde  reqemleri eyni olan ededlerin cemin tapin. example(11 ,222,333 ve  s)
-let sum = 0
-    for (let i = 0; i < 100; i++) {
-        if(i%11===0){
-            sum +=i
-            
-        }
-        
-    }console.log(sum);
+// 2. Verilmiş elementin bir ölçülü massivdə neçə dəfə təkrarlandığını tapan function tərtib edin.
 
-    // --------------------------------------------------task4-----------------------------------------------------------------------------
-    // Verilmis array-de cut ededlerin en boyuyunu tapan alqoritm yazin.
-
-    const arr = [34, 56,23,78,102,133,48]
-    let num = 0
-    for (let i = 0; i < arr.length; i++){
-        if(arr[i]%2===0){
-        if(num<arr[i])
-            num=arr[i]
+function counttekrarlanma(arr, elementToCount) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === elementToCount) {
+        count++;
+      }
     }
-       
-        }
-        console.log(num);
-
-         // --------------------------------------------------task5------------------------------------------------------------------------
-    // Verilmis array-de tek ededlerin en kiciyinin index-ni tapan alqoritm yazin.
-
-    const array =[34, 56,23,78,102,133,48];
-let num1 = array[0]; 
-for (var i = 0; i < array.length; i++) {
-    if(array[i]<num1)
-    {
-    
-        num1 = i;
-    }
-
-}
-  console.log(num1);
-
+    return count;
+  }
+  
+  
+  const myArray = [1, 2, 2, 3, 4, 2, 5, ];
+  const secilenelement = 2;
+  const tekrarlanma = counttekrarlanma(myArray, secilenelement);
+  
+  console.log(tekrarlanma);
   
 
-        
-            
+//3. arrayin  elementləri arasında minimum və maksimum elementi nəzərə almadan yerdə qalan elementlərin cəmini tapan function.
 
         
-    
+function minMaxhesablamadanTopla(arr) {
+    if (arr.length < 3) {
+      return 0; // Əgər massiv 2 və ya daha az elementdən ibarətdirsə, minimum və maksimum olmayacaq, ona görə də cəmi 0 olmalıdır.
+    }
+  
+    let min = Math.min(...arr);
+    let max = Math.max(...arr);
+    let toplam = 0;
+  
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== min && arr[i] !== max) {
+        toplam += arr[i];
+      }
+    }
+  
+    return toplam;
+  }
+  
+  
+  const massiv = [3,12,56,43,22,78];
+  const netice = minMaxhesablamadanTopla(massiv);
+  
+  console.log(netice);
+  
 
-    
+//1.Verilmis arrayin icerisinde deyeri 45 olan elementin indexini tapan function.
+
+
+function indeksitap(arr, value) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === value) {
+        return i;
+      }
+    }
+     
+  }
+  
+  
+  const array = [ 12,32,44,56,45];
+  const value = 45;
+  const indeks = indeksitap(array, value);
+  
+   
+    console.log(indeks);
+  
+  
