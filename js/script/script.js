35a7841b16e8acc72df48657b9426007fa35adc7
mahammadmehdi <mi7u4p9f7@code.edu.az>
2023-10-27 10:09:36 +0400

    string

--- js/script/script.js
+++ js/script/script.js
@@ -1,69 +1,40 @@
-// 2. Verilmiş elementin bir ölçülü massivdə neçə dəfə təkrarlandığını tapan function tərtib edin.
+// 17.Verilmiş ədədin bölənlərinin sayını tapın
 
-function counttekrarlanma(arr, elementToCount) {
-    let count = 0;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === elementToCount) {
-        count++;
-      }
-    }
-    return count;
-  }
+function bolenlerinSayi(say) {
+    let bolenSayi = 0;
   
-  
-  const myArray = [1, 2, 2, 3, 4, 2, 5, ];
-  const secilenelement = 2;
-  const tekrarlanma = counttekrarlanma(myArray, secilenelement);
-  
-  console.log(tekrarlanma);
-  
-
-//3. arrayin  elementləri arasında minimum və maksimum elementi nəzərə almadan yerdə qalan elementlərin cəmini tapan function.
-
-        
-function minMaxhesablamadanTopla(arr) {
-    if (arr.length < 3) {
-      return 0; // Əgər massiv 2 və ya daha az elementdən ibarətdirsə, minimum və maksimum olmayacaq, ona görə də cəmi 0 olmalıdır.
-    }
-  
-    let min = Math.min(...arr);
-    let max = Math.max(...arr);
-    let toplam = 0;
-  
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] !== min && arr[i] !== max) {
-        toplam += arr[i];
+    for (let i = 1; i <= say; i++) {
+      if (say % i === 0) {
+        bolenSayi++;
       }
     }
   
-    return toplam;
+    return bolenSayi;
   }
   
+ 
   
-  const massiv = [3,12,56,43,22,78];
-  const netice = minMaxhesablamadanTopla(massiv);
-  
-  console.log(netice);
+  console.log(bolenlerinSayi(18));
   
 
-//1.Verilmis arrayin icerisinde deyeri 45 olan elementin indexini tapan function.
-
-
-function indeksitap(arr, value) {
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === value) {
-        return i;
+//  18. Verilmiş ədədin bölənlərinin cəmini və hasilini tapın Javascript
+  function bolenlerinCemiVeHasili(say) {
+    let cem = 0;
+    let hasil = 1;
+  
+    for (let i = 1; i <= say; i++) {
+      if (say % i === 0) {
+        cem += i;   
+        hasil *= i; 
       }
     }
-     
+  
+    return { cem, hasil };
   }
   
   
-  const array = [ 12,32,44,56,45];
-  const value = 45;
-  const indeks = indeksitap(array, value);
   
-   
-    console.log(indeks);
+  console.log(bolenlerinCemiVeHasili(12));
+  
   
   
